b77c1859c368c0e6c6839b63e26e5b7edba7256e
jaychopda <jaychopda6372@gmail.com>
2024-12-21 23:57:47 +0530

    main

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-connectDB();
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running at port ${process.env.PORT || 3000}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB Connection Failed!!!",err)
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const aysncHandler = (requestHandler => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+})
+
+export{ aysncHandler }
